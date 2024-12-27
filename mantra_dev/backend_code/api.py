0fb9a577ab87b90f7ce9290eb8620f5c83155c4c
foramshah26 <foram@sanskartechnolab.com>
2024-12-27 15:56:52 +0530

    test

--- mantra_dev/backend_code/api.py
+++ mantra_dev/backend_code/api.py
@@ -22,8 +22,8 @@ from frappe.model.mapper import get_mapped_doc
 # frappe.whitelist()
 # def stop_print_in_draft(doc, method, x):    
 #     if doc.docstatus == 0:  # Draft state
-#         frappe.throw("You cannot print the Delivery Note in Draft state.") 
-# Mantra1
+#         frappe.throw("You cannot print the Delivery Note in Draft state.")
+#
 
 
 # @frappe.whitelist()
