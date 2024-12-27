ed4ad527f1d8f87dc72497aa2a5fecf93db7714f
mantraerp <helpdesk1.erp@mantratec.com>
2024-12-27 16:10:30 +0530

    Update purchase_receipt.py

--- mantra_dev/purchase_receipt.py
+++ mantra_dev/purchase_receipt.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
-# License: GNU General Public License v3. See license.txtMantra
+# License: GNU General Public License v3. See license.txtMan
 
 
 import frappe
