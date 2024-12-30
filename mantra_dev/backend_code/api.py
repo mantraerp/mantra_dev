f5e1f23f89d72d493010f18e7dc723660914e085
mantraerp <helpdesk1.erp@mantratec.com>
2024-12-30 09:59:49 +0530

    Add files via upload
    git auto update

--- mantra_dev/backend_code/api.py
+++ mantra_dev/backend_code/api.py
@@ -73,6 +73,20 @@ def mantra_git_pull_migrate():
     os.popen('sh /home/mantra/mantrastage-bench/ShellScript/mantra_git_migrate.sh')
     return "Git pull run with build"
 
+@frappe.whitelist(allow_guest=True)
+def mantra_git_pull_export_fixture():
+    os.popen('sh /home/mantra/mantrastage-bench/ShellScript/mantra_git_export_fixture.sh')
+    return "Git pull run with export fixture"
+
+@frappe.whitelist(allow_guest=True)
+def mantra_git_pull_bench_build_erpnext():
+    os.popen('sh /home/mantra/mantrastage-bench/ShellScript/mantra_git_build_erpnext.sh')
+    return "Git pull run with bench build errpnext"
+
+@frappe.whitelist(allow_guest=True)
+def mantra_git_pull_bench_build_frappe():
+    os.popen('sh /home/mantra/mantrastage-bench/ShellScript/mantra_git_build_frappe.sh')
+    return "Git pull run with bench build frappe"
 
 
 
