5b93b2a9adf0acea1932db01f98a91bb0c323f25
mantraerp <foram@sanskartechnolab.com>
2025-01-01 17:11:14 +0530

    Auto-commit fixture 2025-01-01 17:11:14

--- mantra_dev/backend_code/gitapi.py
+++ mantra_dev/backend_code/gitapi.py
@@ -9,5 +9,7 @@ def mantra_git_pull_with_url(company_name):
     reply['password'] = "E&rp@_5546"
     reply['git_url'] = "https://mantraerp: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@github.com/mantraerp/mantra_dev.git"
     reply['company'] = company_name
+    reply['GIT_USERNAME'] = "mantraerp"
+    reply['GIT_PAT'] = "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"
 
     return reply
