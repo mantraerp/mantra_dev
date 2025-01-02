68991145e9e76d95daf2628d7bf89c4e88773673
mantraerp <foram@sanskartechnolab.com>
2025-01-02 14:25:05 +0530

    Auto-commit fixture 2025-01-02 14:25:05

--- mantra_dev/Backup/ShellScript/mantra_git_migrate.sh
+++ mantra_dev/Backup/ShellScript/mantra_git_migrate.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-time_stamp=$(date +%Y-%m-%d-%T)
-echo "Build Migrate app start $time_stamp" >> /home/mantra/mantrastage-bench/logs/gitauto.log 2>&1
-cd /home/mantra/mantrastage-bench/
-bench migrate
+# Configuration variables
+API_URL="http://192.168.1.38:8001/api/method/mantra_dev.backend_code.gitapi.mantra_git_pull_with_url"
+
+# Make a REST API request
+response=$(curl -s -X POST "$API_URL" \
+    -H "Content-Type: application/json" \
+    -d "{\"company_name\": \"mantra\"}")
+
+
+# Check if the response was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to fetch password from the API." >> /home/mantra/mantrastage-bench/logs/gitauto.log 2>&1
+    exit 1
+fi
+
+# Parse the password from the response (assumes JSON response)
+password=$(echo "$response" | jq -r '.message.password')
+
+echo "################  Bench migrate start $(date +'%Y-%m-%d %H:%M:%S') ###############" >> /home/mantra/mantrastage-bench/logs/gitauto.log 2>&1
+cd /home/mantra/mantrastage-bench/apps/
+echo $password | sudo -S -k bench migrate
+echo "################  Bench migrate end $(date +'%Y-%m-%d %H:%M:%S') ###############" >> /home/mantra/mantrastage-bench/logs/gitauto.log 2>&1
--- mantra_dev/backend_code/gitpull.py
+++ mantra_dev/backend_code/gitpull.py
@@ -16,19 +16,19 @@ def mantra_git_pull():
 @frappe.whitelist(allow_guest=True)
 def mantra_git_pull_migrate():
     os.popen('sh /home/mantra/mantrastage-bench/ShellScript/mantra_git_migrate.sh')
-    return "Git pull run with build"
+    return "Migrate bench"
 
 @frappe.whitelist(allow_guest=True)
 def mantra_git_pull_export_fixture():
     os.popen('sh /home/mantra/mantrastage-bench/ShellScript/mantra_git_export_fixture.sh')
-    return "Git pull run with export fixture"
+    return "Export fixture > Git push run"
 
 @frappe.whitelist(allow_guest=True)
 def mantra_git_pull_bench_build_erpnext():
     os.popen('sh /home/mantra/mantrastage-bench/ShellScript/mantra_git_build_erpnext.sh')
-    return "Git pull run with bench build errpnext"
+    return "Build ERPNext app"
 
 @frappe.whitelist(allow_guest=True)
 def mantra_git_pull_bench_build_frappe():
     os.popen('sh /home/mantra/mantrastage-bench/ShellScript/mantra_git_build_frappe.sh')
-    return "Git pull run with bench build frappe"
+    return "Build frappe app"
