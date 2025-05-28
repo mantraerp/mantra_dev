a468ab3d9bf66f7c2e595a494ca0467eb820ac68
mantraerp <foram@sanskartechnolab.com>
2025-05-28 13:23:52 +0530

    backup260525

--- mantra_dev/hooks.py
+++ mantra_dev/hooks.py
@@ -18,7 +18,6 @@ app_include_js = [
 doctype_js = {
     "Sales Invoice": "public/js/sales_invoice.js",
     "Bank Account": "public/js/bank_account.js",
-    # "Quality Inspection": "public/js/quality_inspection.js",
     "Expense Claim": "public/js/expense_claim.js",
 }
 
@@ -27,7 +26,7 @@ override_doctype_class = {
     "Material Request": "mantra_dev.material_request.MaterialRequest",
     "Subcontracting Order": "mantra_dev.backend_code.subcontracting.subcontracting_order.SubcontractingOrder",
     "Stock Reservation Entry": "mantra_dev.backend_code.stock_reservation_entry.stock_reservation_entry.StockReservationEntry",
-    "Bank Transaction": "mantra_dev.overrides.bank_transaction.CustomBankTransaction",
+    # "Bank Transaction": "mantra_dev.overrides.bank_transaction.CustomBankTransaction",
 }
 
 # Document Events
@@ -35,25 +34,25 @@ override_doctype_class = {
 # Hook on document methods and events
 
 doc_events = {
-    "Bank Transaction": {
-		"on_update_after_submit": "mantra_dev.overrides.bank_transaction.on_update_after_submit",
-	},
-    "Purchase Order": {
-        "on_submit": "mantra_dev.backend_code.purchase_order.purchase_order.create_purchase_order_expected_date",
-        "on_cancel": "mantra_dev.backend_code.purchase_order.purchase_order.cancel_purchase_order_expected_date"
-    },
-    "Purchase Receipt": {
-        "on_submit": "mantra_dev.backend_code.purchase_receipt.purchase_receipt.update_purchase_order_expected_date",
-        "on_cancel": "mantra_dev.backend_code.purchase_receipt.purchase_receipt.update_cancel_purchase_order_expected_date" 
-    },
-    "Purchase Invoice":{
-        "on_update": "mantra_dev.purchase_invoice.override_validate_due_date",
-    },
-    "Stock Entry":{
-        "on_submit":"mantra_dev.backend_code.qc_module.send_notification_on_submit",
-        "on_cancel":"mantra_dev.backend_code.qc_module.revert_auto_transfer_stock",
-        "on_trash": "mantra_dev.backend_code.qc_module.restore_qc_quantities_on_delete"
-    },
+    # "Bank Transaction": {
+	# 	"on_update_after_submit": "mantra_dev.overrides.bank_transaction.on_update_after_submit",
+	# },
+    # "Purchase Order": {
+    #     "on_submit": "mantra_dev.backend_code.purchase_order.purchase_order.create_purchase_order_expected_date",
+    #     "on_cancel": "mantra_dev.backend_code.purchase_order.purchase_order.cancel_purchase_order_expected_date"
+    # },
+    # "Purchase Receipt": {
+    #     "on_submit": "mantra_dev.backend_code.purchase_receipt.purchase_receipt.update_purchase_order_expected_date",
+    #     "on_cancel": "mantra_dev.backend_code.purchase_receipt.purchase_receipt.update_cancel_purchase_order_expected_date" 
+    # },
+    # "Purchase Invoice":{
+    #     "on_update": "mantra_dev.purchase_invoice.override_validate_due_date",
+    # },
+    # "Stock Entry":{
+    #     "on_submit":"mantra_dev.backend_code.qc_module.send_notification_on_submit",
+    #     "on_cancel":"mantra_dev.backend_code.qc_module.revert_auto_transfer_stock",
+    #     "on_trash": "mantra_dev.backend_code.qc_module.restore_qc_quantities_on_delete"
+    # },
 }
 
 # Scheduled Tasks
@@ -94,4 +93,5 @@ fixtures = [
 ]
 
 
-get_matching_queries = "mantra_dev.mantra_dev.doctype.bank_reconciliation_tool_mantra.bank_reconciliation_tool_mantra.get_matching_queries"
+# get_matching_queries = "mantra_dev.mantra_dev.doctype.bank_reconciliation_tool_mantra.bank_reconciliation_tool_mantra.get_matching_queries"
+# get_matching_queries = "mantra.mantra.doctype.bank_reconciliation_tool_mantra.bank_reconciliation_tool_mantra.get_matching_queries"
