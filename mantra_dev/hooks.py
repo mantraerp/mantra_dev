af8427a2458176e4f35fc7eeee640cbbd885aa65
mantraerp <ravi.patel@mantratec.com>
2026-02-18 17:34:47 +0530

    hook change

--- mantra_dev/hooks.py
+++ mantra_dev/hooks.py
@@ -22,7 +22,7 @@ scheduler_events = {
             "mantra_dev.backend_code.avdm.process_one_record"
         ],        
         "0/5 * * * *": [
-            "mantra_dev.api_code.bank_transaction.get_icici_bank_file",
+            # "mantra_dev.api_code.bank_transaction.get_icici_bank_file",
         ],
         "30 22 * * *": [
             "mantra_dev.backend_code.serialno.serial_no_scheduled"
